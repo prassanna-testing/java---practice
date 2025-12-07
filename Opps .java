5f2af5e35b95b8338d52efff6fea051f286d6497
prassanna-testing <gundhadapremsira05@gmail.com>
2025-12-07 18:26:10 +0530

    Add files via upload

---
+++ Opps .java
@@ -0,0 +1,11 @@
+public class mobile {
+  String color = "seagreen";
+  int speed = 120;
+
+  public static void main(String[] args) {
+    mobile myMobile = new mobile();
+    System.out.println("Car color: " + myMobile.color);
+    System.out.println("Car speed: " + myMobile.speed);
+  }
+}
+
